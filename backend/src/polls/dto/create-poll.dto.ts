1db0c3efb3b8657958802ecb5f5ec3190b360b56
ksamrat224 <ksamrat224@gmail.com>
2025-04-22 21:20:28 +0545

    validator for poll added

--- backend/src/polls/dto/create-poll.dto.ts
+++ backend/src/polls/dto/create-poll.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePollDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, Length } from "class-validator";
+
+export class CreatePollDto {
+    @IsNotEmpty()
+    @IsString()
+    @Length(5,50)
+    title:string;
+    
+    @IsNotEmpty()
+    @IsString()
+    @Length(10,200)
+    description:string;
+    
+    @IsOptional()
+    @IsBoolean()
+    isActive:boolean;
+}
