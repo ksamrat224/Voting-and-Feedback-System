1986950526d7ca95d712ee7bdc84ece85bbdbced
ksamrat224 <ksamrat224@gmail.com>
2025-04-26 20:50:05 +0545

    polloption dto created

--- backend/src/poll-options/dto/create-poll-option.dto.ts
+++ backend/src/poll-options/dto/create-poll-option.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePollOptionDto {}
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
+
+export class CreatePollOptionDto {
+    @IsNotEmpty()
+    @IsString()
+    text: string;
+
+    @IsNotEmpty()
+    @IsInt()
+    pollId: number;
+}
