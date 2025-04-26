4a4aa299e92beb373197dbc79688f63a86a942ad
ksamrat224 <ksamrat224@gmail.com>
2025-04-26 20:50:58 +0545

    prismaclient dependency added in polloptions module

--- backend/src/poll-options/poll-options.module.ts
+++ backend/src/poll-options/poll-options.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PollOptionsService } from './poll-options.service';
 import { PollOptionsController } from './poll-options.controller';
+import { PrismaClient } from 'generated/prisma';
 
 @Module({
   controllers: [PollOptionsController],
-  providers: [PollOptionsService],
+  providers: [PollOptionsService,PrismaClient],
 })
 export class PollOptionsModule {}
