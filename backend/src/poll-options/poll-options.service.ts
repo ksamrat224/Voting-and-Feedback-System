01423b30a68cb678a7b600b7c7c17a1c8785a61f
ksamrat224 <ksamrat224@gmail.com>
2025-04-26 20:56:30 +0545

    findall and findone opeeration completed

--- backend/src/poll-options/poll-options.service.ts
+++ backend/src/poll-options/poll-options.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePollOptionDto } from './dto/create-poll-option.dto';
 import { UpdatePollOptionDto } from './dto/update-poll-option.dto';
 import { PrismaClient } from 'generated/prisma';
@@ -6,18 +6,24 @@ import { PrismaClient } from 'generated/prisma';
 @Injectable()
 export class PollOptionsService {
   constructor(private readonly prisma: PrismaClient) {}
- async create(createPollOptionDto: CreatePollOptionDto) {
+  async create(createPollOptionDto: CreatePollOptionDto) {
     return this.prisma.pollOption.create({
       data: createPollOptionDto,
     });
   }
 
-  findAll() {
-    return `This action returns all pollOptions`;
+  async findAll() {
+    return this.prisma.pollOption.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pollOption`;
+  async findOne(id: number) {
+    const pollOption = await this.prisma.pollOption.findUnique({
+      where: { id },
+    });
+    if (!pollOption) {
+      throw new NotFoundException('Poll option not found');
+    }
+    return pollOption;
   }
 
   update(id: number, updatePollOptionDto: UpdatePollOptionDto) {
