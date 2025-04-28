b6d034396aa489bb3b18553f6653ca94bbb4487d
ksamrat224 <ksamrat224@gmail.com>
2025-04-28 16:32:33 +0545

    dto of votes created

--- backend/src/votes/dto/create-vote.dto.ts
+++ backend/src/votes/dto/create-vote.dto.ts
@@ -1,1 +1,11 @@
-export class CreateVoteDto {}
+import { IsInt, IsNotEmpty } from "class-validator";
+
+export class CreateVoteDto {
+    @IsInt()
+    @IsNotEmpty()
+    userId:number;
+    
+    @IsInt()
+    @IsNotEmpty()
+    pollOptionId:number;
+}
