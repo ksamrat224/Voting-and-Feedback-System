2ce4580f9b706cb15006529a6c514a14bfa80a12
ksamrat224 <ksamrat224@gmail.com>
2025-04-28 16:20:03 +0545

    resource for vote created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { PollsModule } from './polls/polls.module';
 import { PolloptionsModule } from './polloptions/polloptions.module';
 import { PollOptionsModule } from './poll-options/poll-options.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [UsersModule, PollsModule, PolloptionsModule, PollOptionsModule],
+  imports: [UsersModule, PollsModule, PolloptionsModule, PollOptionsModule, VotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
