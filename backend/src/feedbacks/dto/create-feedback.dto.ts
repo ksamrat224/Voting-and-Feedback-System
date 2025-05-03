c53e6b74f8c8078058fb6ecfe207ec89afddf704
ksamrat224 <ksamrat224@gmail.com>
2025-05-03 16:24:49 +0545

    feedback dto created

--- backend/src/feedbacks/dto/create-feedback.dto.ts
+++ backend/src/feedbacks/dto/create-feedback.dto.ts
@@ -1,1 +1,11 @@
-export class CreateFeedbackDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateFeedbackDto {
+  @IsString()
+  @IsNotEmpty()
+  message:string;
+
+  @IsInt()
+  @IsOptional()
+  userId?: string;
+}
