2a15e17fb16fcd954b7a7a3266391aaa26513bae
ksamrat224 <ksamrat224@gmail.com>
2025-05-03 16:26:33 +0545

    prismaclient dependency added into feedback module

--- backend/src/feedbacks/feedbacks.module.ts
+++ backend/src/feedbacks/feedbacks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FeedbacksService } from './feedbacks.service';
 import { FeedbacksController } from './feedbacks.controller';
+import { PrismaClient } from 'generated/prisma';
 
 @Module({
   controllers: [FeedbacksController],
-  providers: [FeedbacksService],
+  providers: [FeedbacksService,PrismaClient],
 })
 export class FeedbacksModule {}
